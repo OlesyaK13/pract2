744bab8c44eea1c9380ec66e9ae29525903b1aff
OlesyaK13 <115469787+OlesyaK13@users.noreply.github.com>
2024-04-02 19:47:32 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+from FileManager import FileManager
+from pathlib import Path
+"""
+Файл настроек
+"""
+# MY_PATH Рабочая папка FileManager
+MY_PATH = "Home"
+# FM_PATH Рабочая директория FileManager
+FM_PATH = Path(MY_PATH).absolute()
